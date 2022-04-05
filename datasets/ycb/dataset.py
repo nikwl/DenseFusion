1c928a1dd608528278636a54e93defba38995564
nikwl <nikolas.lamb@gmail.com>
2022-04-04 23:27:05 -0400

    d

--- datasets/ycb/dataset.py
+++ datasets/ycb/dataset.py
@@ -171,9 +171,9 @@ class PoseDataset(data.Dataset):
     def load_npy(self):
         print("Loading numpy data ...")
         self.list_rgb = np.load(self.root + "/list_rgb.npy")
-        print("list_rgb: {}".format(len(self.list_rgb)))
+        print("list_rgb: {}".format(lself.list_rgb.shape[-1]))
         print("list: {}".format(len(self.list)))
-        assert len(self.list) == len(self.list_rgb)
+        assert len(self.list) == self.list_rgb.shape[-1]
         self.list_depth = np.load(self.root + "/list_depth.npy")
         self.list_label = np.load(self.root + "/list_label.npy")
         self.list_meta = np.load(self.root + "/list_meta.npy")
