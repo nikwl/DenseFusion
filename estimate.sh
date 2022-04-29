987c42e57cd4341d97ad15c0d0585ae7242621c2
nikwl <nikolas.lamb@gmail.com>
2022-04-29 18:25:02 +0000

    updated gitignore

--- estimate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# run the script with all the correct arguments
-python3 ./tools/estimate.py \
-    --input "$1" \
-    --model trained_checkpoints/ycb/pose_model_26_0.012863246640872631.pth \
-    --refine_model trained_checkpoints/ycb/pose_refine_model_69_0.009449292959118935.pth
